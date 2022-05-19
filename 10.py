a3e1172bca7f680b1c10e7c56f4f030e92369fcd
AlexLobach <alexlobatch@yandex.ru>
2022-05-19 22:51:51 +0300

    write + read

--- 10.py
+++ 10.py
@@ -1,4 +1,24 @@
+
+
+
+fruits = [
+    'orange \n',
+    'apple \n',
+    'kiwi \n'   
+]
+
 with open('draft.txt', 'w') as file:
+    file.writelines (fruits)
     file.write('Hello, everyone \n')
     file.write('Hello, everyone \n')
     file.write('Hello, everyone \n')
+
+with open('draft.txt', 'r') as fileread:
+    print (fileread.readline())
+    print (fileread.readline())
+    print (fileread.readline())
+    print (fileread.readline())
+    print (fileread.readline())
+    print (fileread.readline())
+    
+
