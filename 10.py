e33a7e7f0638c33e8f5204d2655d5fc7946609cc
AlexLobach <alexlobatch@yandex.ru>
2022-05-23 22:51:18 +0300

    #3 after pickle

--- 10.py
+++ 10.py
@@ -1,3 +1,5 @@
+import pathlib
+import pickle
 
 
 
@@ -5,8 +7,14 @@ fruits = [
     'orange \n',
     'apple \n',
     'kiwi \n'   
-]
+    ]
 
+cars = {
+    'hatchback': ["Kia cee'd","VW golf", "Opel astra" ],
+    'sedan' : ["VW jetta", "Toyota camry", "Mazda 6"]
+    }
+
+#1 Записать в файл строку, потом её считать
 with open('draft.txt', 'w') as file:
     file.writelines (fruits)
     file.write('Hello, everyone \n')
@@ -20,5 +28,17 @@ with open('draft.txt', 'r') as fileread:
     print (fileread.readline())
     print (fileread.readline())
     print (fileread.readline())
-    
+
+
+
+
+#2 Сериализовать объект простого класса, загрузить его и запустить метод, использующий аттрибут объекта, при помощи pickle
+file_picle = pathlib.Path('for_pickle.txt')
+with file_picle.open('wb') as fp:
+    pickle.dump(cars, fp)
+
+with file_picle.open('rb') as fp:
+    loaded_data = pickle.load(fp)
+    print (loaded_data ['sedan'])
+
 
